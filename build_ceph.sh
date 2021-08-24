0057cf53707b4d4db7def73ddeb8f60572220f02
Holly Casaletto <hollycasaletto@Hollys-MacBook-Pro-2.local>
2021-08-24 14:52:27 -0700

    minor changes to build_ceph.sh

--- build_ceph.sh
+++ build_ceph.sh
@@ -12,12 +12,14 @@ apt-get update
 
 apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y
 
-apt-get install nodejs -y
+apt install nodejs -y
 
-apt-get install npm -y
+apt install npm -y
 
 chmod -R 777 ~/.cache
 
 chmod -R 777 build/src/pybind/mgr/dashboard
+
+cd build
 
 ninja
