40753f2cd8e1ab07420f0a1d2754addc16c920a8
Holly Casaletto <hollycasaletto@Hollys-MacBook-Pro-2.local>
2021-08-24 13:52:03 -0700

    add build-ceph.sh to build ceph with one command

---
+++ build-ceph.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git clone https://github.com/hcasalet/ceph-lsm.git
+
+cd ceph-lsm
+
+git submodule update --init --recursive
+
+apt-get update
+
+./install-deps.sh
+
+./do_cmake.sh -DCMAKE_BUILD_TYPE=RelWithDebInfo
+
+apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y
+
+apt-get install nodejs -y
+
+apt-get install npm -y
+
+chmod -R 777 ~/.cache
+
+chmod -R 777 build/src/pybind/mgr/dashboard
+
+ninja
