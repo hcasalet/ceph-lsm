cd53aebc3b4dc9683c0c0ff06f7221b184470465
Holly Casaletto <hollycasaletto@Hollys-MacBook-Pro-2.local>
2022-05-15 10:12:51 -0700

    revert changes to Kens remote reads test

--- src/cls/test_remote_operations/cls_test_remote_operations.cc
+++ src/cls/test_remote_operations/cls_test_remote_operations.cc
@@ -14,53 +14,15 @@ cls_method_handle_t h_test_write;
 cls_method_handle_t h_test_scatter;
 cls_method_handle_t h_test_gather;
 
-static int test_gather(cls_method_context_t hctx, bufferlist *in, bufferlist *out);
-
 /**
  * read data
  */
 static int test_read(cls_method_context_t hctx, bufferlist *in, bufferlist *out) {
-  JSONParser parser;
-  bool b = parser.parse(in->c_str(), in->length());
-
-  auto *o_rec = parser.find_obj("recursive");
-  ceph_assert(o_rec);
-  std::string recursive = o_rec->get_data_val().str;
-
-  auto *o_pool = parser.find_obj("pool");
-  ceph_assert(o_pool);
-  std::string pool_name = o_pool->get_data_val().str;
-
-  if (recursive.compare("no") == 0) {
-    int r = cls_cxx_read(hctx, 0, 4096, out);
-    if (r < 0) {
-      CLS_ERR("%s: error reading data", __PRETTY_FUNCTION__);
-      return r;
-    }
-  } else {
-      JSONFormatter *formatter = new JSONFormatter(true);
-      formatter->open_object_section("foo");
-      std::set<std::string> src_objects;
-      src_objects.insert("src_object.1");
-      src_objects.insert("src_object.2");
-      src_objects.insert("src_object.3");
-      encode_json("src_objects", src_objects, formatter);
-      encode_json("cls", "test_remote_operations", formatter);
-      encode_json("method", "test_read", formatter);
-      encode_json("pool", pool_name, formatter);
-      encode_json("recursive", "no", formatter);
-      formatter->close_section();
-      in->clear();
-      formatter->flush(*in);
-
-      bufferlist out2;
-      test_gather(hctx, in, &out2);
-
-      CLS_LOG(1, "winter debug: in test_read recursive section, out2 length = %u", out2.length());
-      out->claim_append(out2);
-      CLS_LOG(1, "winter debug: in test_read recursive section, out length = %u", out->length());
+  int r = cls_cxx_read(hctx, 0, 0, out);
+  if (r < 0) {
+    CLS_ERR("%s: error reading data", __PRETTY_FUNCTION__);
+    return r;
   }
-
   return 0;
 }
 
@@ -80,7 +42,6 @@ static int test_write(cls_method_context_t hctx, bufferlist *in, bufferlist *out
  * scatter data to other objects using remote writes
  */
 static int test_scatter(cls_method_context_t hctx, bufferlist *in, bufferlist *out) {
-  CLS_LOG(1, "Holly debug remote writes");
   int r = cls_cxx_scatter_wait_for_completions(hctx);
   if (r == -EAGAIN) {
     // start remote writes
@@ -130,10 +91,6 @@ static int test_scatter(cls_method_context_t hctx, bufferlist *in, bufferlist *o
  */
 static int test_gather(cls_method_context_t hctx, bufferlist *in, bufferlist *out) {
   std::map<std::string, bufferlist> src_obj_buffs;
-
-  hobject_t soid = cls_get_oid(hctx);
-  std::string oid = soid.oid.name;
-
   int r = cls_cxx_get_gathered_data(hctx, &src_obj_buffs);
   if (src_obj_buffs.empty()) {
     // start remote reads
@@ -155,27 +112,23 @@ static int test_gather(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
     ceph_assert(o_pool);
     std::string pool = o_pool->get_data_val().str;
 
-    auto *o_src_objects = parser.find_obj("src_objects_chain");
+    auto *o_src_objects = parser.find_obj("src_objects");
     ceph_assert(o_src_objects);
     auto src_objects_v = o_src_objects->get_array_elements();
     std::set<std::string> src_objects;
     for (auto it = src_objects_v.begin(); it != src_objects_v.end(); it++) {
       std::string oid_without_double_quotes = it->substr(1, it->size()-2);
-      CLS_LOG(1, "Ken remote read: %s", oid_without_double_quotes.c_str());
       src_objects.insert(oid_without_double_quotes);
     }
-    src_objects.insert("src_object.4");
     r = cls_cxx_gather(hctx, src_objects, pool, cls.c_str(), method.c_str(), *in);
   } else {
     // write data gathered using remote reads
-    //int offset = 0;
+    int offset = 0;
     for (std::map<std::string, bufferlist>::iterator it = src_obj_buffs.begin(); it != src_obj_buffs.end(); it++) {
       bufferlist bl= it->second;
-      //r = cls_cxx_write(hctx, offset, bl.length(), &bl);
-      //offset += bl.length();
-      out->claim_append(bl);
+      r = cls_cxx_write(hctx, offset, bl.length(), &bl);
+      offset += bl.length();
     }
-    CLS_LOG(1, "winter debug: in test_gather: out length = %u", out->length());
   }
   return r;
 }
--- src/test/librados/cls_remote_reads.cc
+++ src/test/librados/cls_remote_reads.cc
@@ -21,37 +21,32 @@ TEST(ClsTestRemoteReads, TestGather) {
 
   // create source objects from which data are gathered
   in.append(buf, sizeof(buf));
+  ASSERT_EQ(0, ioctx.write_full("src_object.1", in));
+  in.append(buf, sizeof(buf));
+  ASSERT_EQ(0, ioctx.write_full("src_object.2", in));
+  in.append(buf, sizeof(buf));
   ASSERT_EQ(0, ioctx.write_full("src_object.3", in));
 
   // construct JSON request passed to "test_gather" method, and in turn, to "test_read" method
   JSONFormatter *formatter = new JSONFormatter(true);
   formatter->open_object_section("foo");
   std::set<std::string> src_objects;
-  src_objects.insert("src_object.0");
   src_objects.insert("src_object.1");
   src_objects.insert("src_object.2");
   src_objects.insert("src_object.3");
-  encode_json("src_objects_chain", src_objects, formatter);
+  encode_json("src_objects", src_objects, formatter);
   encode_json("cls", "test_remote_operations", formatter);
   encode_json("method", "test_read", formatter);
   encode_json("pool", pool_name, formatter);
-  encode_json("recursive", "yes", formatter);
   formatter->close_section();
   in.clear();
   formatter->flush(in);
 
   // create target object by combining data gathered from source objects using "test_read" method
-  //ASSERT_EQ(0, ioctx.exec("tgt_object", "test_remote_operations", "test_gather", in, out));
-  int rval;
-  ObjectWriteOperation op;
-  op.exec("test_remote_reads", "test_gather", in, &out, &rval);
-
-  //ioctx.exec("tgt_object", "test_remote_operations", "test_gather", in, out);
-  ASSERT_EQ(0, ioctx.operate("tgt_object", &op, LIBRADOS_OPERATION_RETURNVEC));
-  std::cout << "winter debug: length of out buffer is: " << out.length() << endl;
+  ASSERT_EQ(0, ioctx.exec("tgt_object", "test_remote_operations", "test_gather", in, out));
 
   // read target object and check its size
-  //ASSERT_EQ(3*object_size, ioctx.read("tgt_object", out, 0, 0));
+  ASSERT_EQ(3*object_size, ioctx.read("tgt_object", out, 0, 0));
 
   ASSERT_EQ(0, destroy_one_pool_pp(pool_name, cluster));
 }
